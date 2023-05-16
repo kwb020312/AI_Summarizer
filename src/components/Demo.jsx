542d1bf7bd56e9929e2859eca2f1475734300d06
N S <kwb020312@naver.com>
2023-05-16 22:40:32 +0900

    Feat:Submit Button

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -23,6 +23,13 @@ const Demo = () => {
             required
             className="url_input peer"
           />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
         </form>
       </div>
     </section>
