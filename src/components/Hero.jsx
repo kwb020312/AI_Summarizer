087c1a648284508719e4aa2e5cfbeb222971efcb
N S <kwb020312@naver.com>
2023-05-16 22:18:10 +0900

    Feat:Main Title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,11 @@ const Hero = () => {
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        <span className="orange_gradient">OPENAI GPT-4</span>를 활용하여
+        <br className="max-md:hidden" />
+        복잡한 문장을 요약하세요!
+      </h1>
     </header>
   );
 };
