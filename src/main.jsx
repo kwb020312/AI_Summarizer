c7f760aaf39bf1dee42db9b3a27ce4240db5584a
N S <kwb020312@naver.com>
2023-05-16 22:55:58 +0900

    Feat:redux store 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { store } from "./services/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
