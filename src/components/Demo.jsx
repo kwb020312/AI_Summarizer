26529703e077099571aa6023d5c7b455f8c06035
N S <kwb020312@naver.com>
2023-05-17 21:15:29 +0900

    Feat:LocalStorage 데이터 저장

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,7 +9,19 @@ const Demo = () => {
     summary: "",
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,7 +30,12 @@ const Demo = () => {
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
 
+      const updatedAllArticles = [newArticle, ...allArticles];
+
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
 
