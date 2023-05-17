b96115ed9f6478e95085d9ab634b5f9d157eb17c
N S <kwb020312@naver.com>
2023-05-17 21:20:40 +0900

    Feat:검색기록 남기기

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -72,6 +72,27 @@ const Demo = () => {
             ↵
           </button>
         </form>
+
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        <span className="orange_gradient">OPENAI GPT-4</span>를 활용하여
+        <span className="orange_gradient">OpenAI GPT-4</span>를 활용하여
         <br className="max-md:hidden" />
         복잡한 문장을 요약하세요!
       </h1>
